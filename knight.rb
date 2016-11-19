3ec378ee4753a266949b8258980a8a8c3db679cd
amory-peng <amorypeng@Amorys-MacBook-Air.local>
2016-11-18 19:27:42 -0800

    fixed movetree

--- knight.rb
+++ knight.rb
@@ -4,97 +4,47 @@ class Knight
 
 attr_accessor :position, :visited_positions, :possible_pos, :parent, :root_node, :visited_nodes
 
-  def initialize(pos, size=8)
+  def initialize(pos)
     @position = pos
-    @visited_positions = []
-    @size = size
-    @visited_nodes = []
-    @root_node = PolyTreeNode.new(position)
+    @visited_positions = [pos]
+    build_move_tree
+
   end
 
 
   def new_move_positions(pos)
     new_moves = valid_moves(pos)
-    new_moves.each do |move|
-      if @visited_positions.include?(move)
-        new_moves.delete(move)
-      else
-        @visited_positions << move
-      end
-    end
+    p "visited_pos #{visited_positions}"
+    p "og array #{new_moves}"
+    new_moves.reject! {|move| move if visited_positions.include?(move)}
+    new_moves.each {|move| visited_positions << move}
     new_moves
   end
 
+  # def new_move_positions(pos)
+  #   valid_moves(pos)
+  #     .reject { |new_pos| visited_positions.include?(new_pos) }
+  #     .each { |new_pos| visited_positions << new_pos }
+  # end
 
   def build_move_tree
-    #debugger
-    current_node =  PolyTreeNode.new(position)
-    current_moves = [position]
-    until current_moves.empty?
-      new_current_moves = []
-      current_moves.each do |current_move|
-        valid_moves(current_move).each do |possible_move|
-          next if @visited_positions.include?(possible_move)
-          new_current_moves << possible_move
-          @visited_positions << possible_move
-          new_node = PolyTreeNode.new(possible_move)
-          @visited_nodes << new_node
-          current_node.add_child(new_node)
-          current_node = new_node
-        #  p "#{new_node.value} parent:#{new_node.parent}"#{}" kids: #{new_no.children}"
-#        p visited_nodes
-        end
-      end
-      current_moves = new_current_moves
-    end
-  end
-
-  def find_path(end_pos, node=root_node)
-    queue = [position]
-    path = []
-    return [end_pos] if node.value == end_pos
-    visited_nodes.each do |node|
-      node.children.each do |child|
-        path + find_path(child.value, child)
-      end
-    end
-    path
-  end
-
-
-  def frownyface
    #debugger
+   self.root_node = PolyTreeNode.new(position)
 
-    queue = [PolyTreeNode.new(position)]
+   queue = [root_node]
     until queue.empty?
       current_node = queue.shift
       current_pos = current_node.value
       new_move_positions(current_pos).each do |move_pos|
         new_node = PolyTreeNode.new(move_pos)
-        new_node.parent = current_node
-        queue << new_node unless queue.include?(new_node)
-        p "pos: #{new_node.value} parent: #{new_node.parent.value}"
+        current_node.add_child(new_node)
+        queue << new_node
       end
     end
   end
 
 
-  # def build_move_tree
-  #  #debugger
-  #
-  #   queue = [self]
-  #   until queue.empty?
-  #
-  #     current_node = queue.shift
-  #     current_pos = current_node.position
-  #     new_move_positions(current_pos).each do |move_pos|
-  #       new_node = Knight.new(move_pos,@size)
-  #       current_node.add_pos(new_node) unless @visited_positions.include?(move_pos)
-  #       queue << new_node unless queue.include?(new_node)
-  #       p new_node
-  #     end
-  #   end
-  # end
+
 
   def valid_moves(pos)
     valid_moves = []
@@ -118,23 +68,10 @@ attr_accessor :position, :visited_positions, :possible_pos, :parent, :root_node,
 
   def inbounds?(pos)
     x,y = pos
-    x.between?(0,@size) && y.between?(0,@size)
+    x.between?(0,7) && y.between?(0,7)
   end
 
-  # def parent=(node)
-  #   if @parent
-  #     old_parent = @parent
-  #     old_parent.possible_pos.delete(self)
-  #   end
-  #   @parent = node
-  #   unless @parent.nil? || @parent.possible_pos.include?(self)
-  #     @parent.possible_pos << self
-  #   end
-  # end
 
-  # def add_pos(node)
-  #   @possible_pos << node
-  #   node.parent = self
-  #   node.visited_positions += @visited_positions
-  # end
 end
+
+test = Knight.new([0,0])
